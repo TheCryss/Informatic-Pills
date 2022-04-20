69392be45c68cf2be113f448d41df09cb8d1289d
Bucheli <jose.bucheli@correounivalle.edu.co>
2022-04-20 08:37:58 -0500

    Usando constantes y Operadores

--- Primeros pasos/src/ConstantesYoperadores.java
+++ Primeros pasos/src/ConstantesYoperadores.java
@@ -13,7 +13,14 @@ public class ConstantesYoperadores {
         double a=5;
         double b =7;
         double c =b/a;
+        final int constante = 3; //esto es una constante de tipo entero
+        int d,e,f;
+        d=1;
+        e=2;
+        f=constante+d+e;
+        
         System.out.println(c);
+        System.out.println("La suma de "+constante +" + "+e+ " + "+d+ " = "+f);
     }
    
 }
